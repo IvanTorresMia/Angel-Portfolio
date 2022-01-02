54595782db17a0650cf61105e6bef7d684ef52ac
Ivan Torres <ivantorresmia27@gmail.com>
2022-01-02 12:35:40 -0800

    atarter code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Nav from './Components/Nav';
+import Header from './/Components/Header'
+
+function App() {
+  return (
+    <div >
+      <Nav />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
