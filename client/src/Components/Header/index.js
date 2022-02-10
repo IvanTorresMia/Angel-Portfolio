f560292be06d07a5386cfff79e65d8a1da35c100
Ivan Torres <ivantorresmia27@gmail.com>
2022-02-09 20:40:10 -0800

    header jsx

---
+++ client/src/Components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="layer">
+        <div className="empty"></div>
+        <div className="header-copy">
+          <h5>This is where you life changes</h5>
+          <h1>Personal Training with Angel Torres</h1>
+          <h2>Let me help you reach your fitness goals</h2>
+          <button>Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
