8fd0ff4caa88c2cff31a5f23963b7496cb1d1762
Ivan Torres <ivantorresmia27@gmail.com>
2022-02-11 02:14:45 -0800

    adding context admin states

--- client/src/Utils/ProgramsContext.js
+++ client/src/Utils/ProgramsContext.js
@@ -4,20 +4,34 @@ import axios from 'axios';
 export const ProgramsContext = createContext();
 
 export const ProgramsProvider = (props) => {
-
+    const applicantsUrl = "http://localhost:3001/applicants";
+    const adminUrl = "http://localhost:3001/admin";
     const [ applicants, setApplicants ] = useState([]);
 
 
     function addApplicant(applicant) {
-        console.log(applicant)
+        return axios.post(applicantsUrl, applicant).then(res => {
+            console.log(res)
+        });
+
     };
+
+    function addOrCreateUser(user) {
+        
+    }
+
+    
+
+
+
 
 
 
     return (
         <ProgramsContext.Provider
             value={{
-                addApplicant
+                addApplicant,
+                addOrCreateUser
             }}
         >
             {props.children}
