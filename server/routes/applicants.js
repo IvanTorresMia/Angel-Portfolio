b6acbd9e0eba74b623dd02e5ddc81a88908a1568
Ivan Torres <ivantorresmia27@gmail.com>
2022-02-11 02:12:37 -0800

    applicants model

---
+++ server/routes/applicants.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
+var cors = require("cors");
+
+
+router.use(cors());
+
+router.post("/", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  console.log(req.body);
+
+
+  models.applicants.create({
+    FirstName: req.body.FirstName,
+    LastName:  req.body.lastName,
+    PhoneNumber:  req.body.phoneNumber,
+    Height: req.body.height,
+    weight:  req.body.weight,
+    GoalWeight: req.body.goalWeight,
+    GoalDescription:  req.body.goalDescription,
+    Program: req.body.program
+  }).then(response => {
+      res.json(response);
+  }).catch((err) => {
+      console.log(err)
+      res.status(500).send();
+      res.statusMessage(err)
+      
+  })
+});
+
+module.exports = router;
